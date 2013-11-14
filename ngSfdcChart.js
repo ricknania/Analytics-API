c6ae55228920da8b6dd7a3619109aa339902c377
arun-sfdc <arun_sfdc@yahoo.com>
2013-11-14 11:43:53 -0800

    Update ngSfdcChart.js

--- ngSfdcChart.js
+++ ngSfdcChart.js
@@ -5,7 +5,6 @@
 // <script src="//cdnjs.cloudflare.com/ajax/libs/graphael/0.5.1/g.raphael-min.js"></script>
 // <script src="//cdnjs.cloudflare.com/ajax/libs/graphael/0.5.1/g.dot-min.js"></script>
 var sfdcCharts = angular.module('sfdcCharts', []);
-google.load("visualization", "1", {packages:["corechart"]});
 sfdcCharts.directive('sfdcChart', function() {
     return {
         restrict: 'E',
