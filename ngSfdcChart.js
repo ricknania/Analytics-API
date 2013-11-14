ef8ac9b860ba2ed4637d7d7fcce597fd5aef0cd3
arun-sfdc <arun_sfdc@yahoo.com>
2013-11-14 12:34:52 -0800

    Update ngSfdcChart.js

--- ngSfdcChart.js
+++ ngSfdcChart.js
@@ -6,60 +6,62 @@
 // <script src="//cdnjs.cloudflare.com/ajax/libs/graphael/0.5.1/g.dot-min.js"></script>
 google.load("visualization", "1", {packages:["corechart"]});
 var sfdcCharts = angular.module('sfdcCharts', []);
-sfdcCharts.directive('sfdcChart', function() {
-    return {
-        restrict: 'E',
-        scope: {
-            data: '=data',
-            type: '=type'
-        },
-        template: '<div style="height: 400px; width: 400px;"></div>',
-        link: function(scope, elem, attrs) {
-            if(scope.type === 'pie') {
-              var chartPoints = [];
-              chartPoints.push(['Grouping', 'Metric']);
-              angular.forEach(scope.data.groupingsDown.groupings, function (value, index) {
-                  chartPoints.push([value.label, scope.data.factMap[value.key + "!T"].aggregates[0].value]);
-              });
-              var myData = google.visualization.arrayToDataTable(chartPoints);
-              new google.visualization.PieChart(elem.children()[0]).draw(myData, {});
-            } else if(scope.type === 'dot') {
-                var valuesx = [];
-                var labelsx = new Array(scope.data.groupingsDown.groupings.length);
-                var valuesy = [];
-                var labelsy = new Array(scope.data.groupingsAcross.groupings.length);
-                var size = [];
-                $.each(scope.data.groupingsDown.groupings, function(di, de) {
-                    $.each(scope.data.groupingsAcross.groupings, function(ai, ae) {
-                        valuesx.push(di);
-                        labelsx[di] = de.label;
-                        valuesy.push(ai);
-                        labelsy[ai] = ae.label;
-                        size.push((scope.data.factMap[de.key+"!"+ae.key].aggregates[0].value));
+sfdcCharts.run(function() {
+    sfdcCharts.directive('sfdcChart', function() {
+        return {
+            restrict: 'E',
+            scope: {
+                data: '=data',
+                type: '=type'
+            },
+            template: '<div style="height: 400px; width: 400px;"></div>',
+            link: function(scope, elem, attrs) {
+                if(scope.type === 'pie') {
+                  var chartPoints = [];
+                  chartPoints.push(['Grouping', 'Metric']);
+                  angular.forEach(scope.data.groupingsDown.groupings, function (value, index) {
+                      chartPoints.push([value.label, scope.data.factMap[value.key + "!T"].aggregates[0].value]);
+                  });
+                  var myData = google.visualization.arrayToDataTable(chartPoints);
+                  new google.visualization.PieChart(elem.children()[0]).draw(myData, {});
+                } else if(scope.type === 'dot') {
+                    var valuesx = [];
+                    var labelsx = new Array(scope.data.groupingsDown.groupings.length);
+                    var valuesy = [];
+                    var labelsy = new Array(scope.data.groupingsAcross.groupings.length);
+                    var size = [];
+                    $.each(scope.data.groupingsDown.groupings, function(di, de) {
+                        $.each(scope.data.groupingsAcross.groupings, function(ai, ae) {
+                            valuesx.push(di);
+                            labelsx[di] = de.label;
+                            valuesy.push(ai);
+                            labelsy[ai] = ae.label;
+                            size.push((scope.data.factMap[de.key+"!"+ae.key].aggregates[0].value));
+                        });
                     });
-                });
-                var r = Raphael("chart");
-                r.dotchart(10, 10, 600, 480, valuesx, valuesy, size, 
-                    {
-                        symbol: "o", 
-                        max: 20, 
-                        heat: true, 
-                        axis: "0 0 1 1", 
-                        axisxstep: scope.data.groupingsDown.groupings.length - 1, 
-                        axisystep: scope.data.groupingsAcross.groupings.length - 1, 
-                        axisxlabels: labelsx, 
-                        axisxtype: " ", 
-                        axisytype: " ", 
-                        axisylabels: labelsy
-                    }
-                ).hover(function () {
-                        this.marker = this.marker || r.tag(this.x, this.y, this.value, 0, this.r + 2).insertBefore(this);
-                        this.marker.show();
-                    }, function () {
-                        this.marker && this.marker.hide();
-                    }
-                );
+                    var r = Raphael("chart");
+                    r.dotchart(10, 10, 600, 480, valuesx, valuesy, size, 
+                        {
+                            symbol: "o", 
+                            max: 20, 
+                            heat: true, 
+                            axis: "0 0 1 1", 
+                            axisxstep: scope.data.groupingsDown.groupings.length - 1, 
+                            axisystep: scope.data.groupingsAcross.groupings.length - 1, 
+                            axisxlabels: labelsx, 
+                            axisxtype: " ", 
+                            axisytype: " ", 
+                            axisylabels: labelsy
+                        }
+                    ).hover(function () {
+                            this.marker = this.marker || r.tag(this.x, this.y, this.value, 0, this.r + 2).insertBefore(this);
+                            this.marker.show();
+                        }, function () {
+                            this.marker && this.marker.hide();
+                        }
+                    );
+                }
             }
-        }
-    };
+        };
+    });
 });
