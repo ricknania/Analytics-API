9778056b3c508fd7dc6dd0597f3b8427b6d9aad0
arun-sfdc <arun_sfdc@yahoo.com>
2013-11-15 10:28:59 -0800

    Update ngSfdcChart.js

--- ngSfdcChart.js
+++ ngSfdcChart.js
@@ -4,6 +4,9 @@
 // <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.2/raphael-min.js"></script>
 // <script src="//cdnjs.cloudflare.com/ajax/libs/graphael/0.5.1/g.raphael-min.js"></script>
 // <script src="//cdnjs.cloudflare.com/ajax/libs/graphael/0.5.1/g.dot-min.js"></script>
+// <script src="//cdnjs.cloudflare.com/ajax/libs/d3/3.3.3/d3.min.js"></script>
+// <script src="//cdnjs.cloudflare.com/ajax/libs/nvd3/1.0.0-beta/nv.d3.min.js"></script>
+ 
 google.load("visualization", "1", {packages:["corechart"]});
 angular.module('sfdcCharts', []).config(function($provide, $compileProvider, $filterProvider) {
     $compileProvider.directive('sfdcChart', function() {
@@ -59,6 +62,17 @@ angular.module('sfdcCharts', []).config(function($provide, $compileProvider, $fi
                             this.marker && this.marker.hide();
                         }
                     );
+                } else if(scope.type === 'bar') {
+                    var chart = nv.models.multiBarChart();
+                    var chartData = [];
+                    angular.forEach(scope.data.groupingsDown.groupings, function(de, di) {
+                        var values = [];
+                        chartData.push({"key":de.label, "values": values});
+                        angular.forEach(de.groupings, function(ae, ai) {
+                            values.push({"x": ae.label, "y": scope.data.factMap[de.key+"!"+ae.key].aggregates[0].value});
+                        });
+                    });
+                    d3.select(elem.children()[0]).datum(chartData).transition().duration(500).call(chart);
                 }
             }
         };
